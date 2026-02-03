30f15ae5653c03bd77b8761c0619e0ba11f0e5c5
Rito Sarkar <ritosarkar79@gmail.com>
2026-02-04 05:02:05 +0530

    pre and post filter code changes

--- src/main/java/com/springSecurity/controller/ContactController.java
+++ src/main/java/com/springSecurity/controller/ContactController.java
@@ -4,12 +4,15 @@ package com.springSecurity.controller;
 import com.springSecurity.model.Contact;
 import com.springSecurity.repository.ContactRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -20,16 +23,24 @@ public class ContactController {
 
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact){
-       contact.setContactId(getServiceReqNumber());
-       contact.setCreateDt(new Date(System.currentTimeMillis()));
-       return contactRepository.save(contact);
+    //@PreFilter("filterObject.contactName != 'Test'")
+    @PostFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        List<Contact> returnContactList = new ArrayList<>();
+        if (!contacts.isEmpty()) {
+            Contact contact = contacts.getFirst();
+            contact.setContactId(getServiceReqNumber());
+            contact.setCreateDt(new Date(System.currentTimeMillis()));
+            Contact savedContact = contactRepository.save(contact);
+            returnContactList.add(savedContact);
+        }
+        return returnContactList;
     }
 
-    public String getServiceReqNumber(){
-        Random random=new Random();
-        int ranNumber =random.nextInt(999999999-9999)+9999;
-        return "SR"+ranNumber;
+    public String getServiceReqNumber() {
+        Random random = new Random();
+        int ranNumber = random.nextInt(999999999 - 9999) + 9999;
+        return "SR" + ranNumber;
 
     }
 
